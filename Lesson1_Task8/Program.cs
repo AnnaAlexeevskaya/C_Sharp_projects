a907802822dbc59d072ba15f2f70367c2ebb15e8
AnnaA <alexeevskayaas@gmail.com>
2023-08-03 20:43:40 +0300

    Task8 ready for check

--- Lesson1_Task8/Program.cs
+++ Lesson1_Task8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 2) {Console.WriteLine($"Введено некорректное число либо нет четных чисел до {N} включительно");}
+
+else
+{
+    int i =2;   
+
+    while (i <= N)
+       {
+      Console.Write($"{i} ");
+      i=i+2;
+       }
+
+ }
